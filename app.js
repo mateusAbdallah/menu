78578d7b5c653913139de1f51ed8f8c894f59585
Mateus Abdallah <abdallahmateus@gmail.com>
2022-07-12 21:11:26 -0300

    setting functions in the buttons and become interactive

--- app.js
+++ app.js
@@ -98,31 +98,12 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
+const container = document.querySelector(".btn-container");
+
 
 window.addEventListener("DOMContentLoaded", function(){
     displayMenuItems(menu);
-    const categories = menu.map(function(item){
-        return item.category;
-    })
-    console.log(categories);
-})
-
-filterBtns.forEach(function(btn){
-    btn.addEventListener("click", function(e){
-        const category = e.currentTarget.dataset.id;
-        const menuCategory = menu.filter(function(menuItems){
-            if (menuItems.category === category){
-                return menuItems;
-            }
-        });
-        if (category === "All"){
-            displayMenuItems(menu);
-        }
-        else {
-            displayMenuItems(menuCategory);
-        }
-    })
+    displayMenuButtons();
 })
 
 function displayMenuItems(menuItems){
@@ -142,4 +123,36 @@ function displayMenuItems(menuItems){
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons(){
+    const categories = menu.reduce(function(values, item){
+        if(!values.includes(item.category)){
+            values.push(item.category);
+        }
+        return values
+    }, 
+    ["All"]
+    );
+    const categoryBtns = categories.map(function(category){
+        return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+    }).join('');
+    container.innerHTML = categoryBtns; 
+    const filterBtns = document.querySelectorAll(".filter-btn");
+    filterBtns.forEach(function(btn){
+        btn.addEventListener("click", function(e){
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = menu.filter(function(menuItems){
+                if (menuItems.category === category){
+                    return menuItems;
+                }
+            });
+            if (category === "All"){
+                displayMenuItems(menu);
+            }
+            else {
+                displayMenuItems(menuCategory);
+            }
+        })
+    })
 }
